17f2e38ae4efbd9bd33c9477c9f653f2c92d9ed9
arnanda18 <57783998+arnanda18@users.noreply.github.com>
2022-06-08 19:53:47 +0700

    Update login_form_view.php

--- CodeIgniter3/Prak 8/login_form_view.php
+++ CodeIgniter3/Prak 8/login_form_view.php
@@ -1,1 +1,25 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Admin Login</title>
+</head>
+<body>
+	
+	<h2>Admin Login</h2>
+	
+	<h4 style="color: red;">
+		<?php 
+		if (isset($error_message)){
+			echo $error_message;
+		}
+		echo validation_errors();
+	?>
+	</h3>
+	<form action="<?php echo base_url()?>index.php/login" method="POST">
+		Username : <br> <input type="text" name="username" size="30"><br>
+		Password : <br><input type="password" name="password" size="30"><br>
+		<input type="submit" name="btnSubmit" value="Login">
+	</form>
+</body>
+</html>
