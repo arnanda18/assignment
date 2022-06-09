6b60f52c10365af517db0006c514c34e549b522b
arnanda18 <57783998+arnanda18@users.noreply.github.com>
2022-06-09 09:44:23 +0700

    Update Hello.php

--- CodeIgniter3/Prak 7/Hello.php
+++ CodeIgniter3/Prak 7/Hello.php
@@ -1,1 +1,16 @@
-
+<?php
+	class Hello extends CI_Controller
+	{ 
+		public function index() {
+			$this->load->model('Hello_model');
+			$model = $this->Hello_model;
+			$s = $model->string;
+			$data = ['text'=>$s];
+			$data['stringNIM'] = '191110227';
+			$data['stringNama'] = 'Arnanda Nuryasa';
+			$data['stringMsg'] = 'Ini adalah kodingan versi saya menggunakan CodeIniter 3. Sederhana sih, ngikut modul saja'; 
+			$this->load->view('hello_view', $data, ['text' =>$s]);
+			
+		}
+}
+?>
