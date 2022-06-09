784764d889abdc2ddb02e47847748493a547d2b5
arnanda18 <57783998+arnanda18@users.noreply.github.com>
2022-06-09 09:45:38 +0700

    Create hello_view.php

---
+++ CodeIgniter3/Prak 7/hello_view.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>MVC</title>
+
+	<style type="text/css">
+		body{
+			text-align: center;
+			font-size: 16px;
+		}
+		h3{
+			color: red;
+			font-size: 22px;
+		}
+	</style>
+</head>
+<body>
+	<h2>MVC</h2>
+	<p><?php echo $text; ?></p>
+	<p><?php echo $stringNIM; ?></p>
+	<p><?php echo $stringNama; ?></p>
+	<h3><?php echo $stringMsg; ?></h3>
+</body>
+</html>
